b1d33b65b7af3e9ce332a49b56b0953a6dcbb580
Markus Oppel <markus.oppel@univie.ac.at>
2020-04-15 09:14:09 +0200

    changed travis ci yml file

--- source/hueckel.f90
+++ source/hueckel.f90
@@ -24,6 +24,9 @@ do i=1,natoms
         print 200,i,element(i),coordinates(i,1:3)
 enddo
 
+print *
+print *,"Happy landing"
+
 100 format(X,'NATOM',X,'Element',3X,'X',9X,'Y',9X'Z')
 200 format(X,I3,4X,A2,3X,3F10.5)
 

39f33b1bbf9bd6dacc99e57fac1123f4af8137cf
Markus Oppel <markus.oppel@univie.ac.at>
2020-04-19 15:32:46 +0200

    added setup of hueckel matrix

--- source/hueckel.f90
+++ source/hueckel.f90
@@ -7,14 +7,43 @@ character(len=80)::title
 character(len=2),dimension(maxatoms)::element
 real(kind=8),dimension(maxatoms,3)::coordinates
 
-integer::i
+real(kind=8),dimension(maxatoms,maxatoms)::hmatrix=0.0
+real(kind=8),dimension(3)::dij
 
+real(kind=8)::alpha,beta
+real(kind=8)::rij,dmax
+
+integer::i,j
+
+alpha=0.0
+beta=1.0
+dmax=1.4
 
 read(*,*) natoms
 read(*,*) title
 do i=1,natoms
         read(*,*) element(i),coordinates(i,1:3)
 enddo
+
+! Setup the hueckel matrix
+
+do i=1,natoms
+ if (element(i).eq."C") hmatrix(i,i)=alpha 
+ do j=i+1,natoms
+   if (element(i).eq."C") then
+       if(element(i).eq.element(j)) then
+         dij=coordinates(i,1:3)-coordinates(j,1:3)
+         rij=sqrt(dot_product(dij,dij)) 
+         if (rij.le.dmax) then
+                hmatrix(i,j)=beta
+                hmatrix(j,i)=hmatrix(i,j)
+         endif
+        endif
+   endif        
+ enddo
+enddo 
+
+
 
 print *,"Input structure"
 print *
@@ -23,6 +52,13 @@ print 100
 do i=1,natoms
         print 200,i,element(i),coordinates(i,1:3)
 enddo
+
+
+print *,"Hueckel matrix"
+print *
+do i=1,natoms
+        print *,i,hmatrix(i,1:natoms)
+enddo 
 
 print *
 print *,"Happy landing"

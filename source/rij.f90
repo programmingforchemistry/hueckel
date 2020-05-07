ee25d79a729cc6a8551b5b454009ef2be5e14aae
Markus Oppel <markus.oppel@univie.ac.at>
2020-05-07 20:51:39 +0200

    added rij function

---
+++ source/rij.f90
@@ -0,0 +1,13 @@
+real(kind=8) function rij(i,j)
+use global
+implicit none
+
+integer::i,j
+
+real(kind=8),dimension(3)::dij
+
+dij=coordinates(i,1:3)-coordinates(j,1:3)
+rij=sqrt(dot_product(dij,dij))
+
+return
+end function rij

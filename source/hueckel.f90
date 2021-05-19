64f39cee975ae4aa3c2f8d515d81bd4e66f986da
Markus Oppel <markus.oppel@univie.ac.at>
2021-05-19 11:40:01 +0200

    Update hueckel.f90
    added short license text

--- source/hueckel.f90
+++ source/hueckel.f90
@@ -1,3 +1,21 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!                                                                      !
+! Hueckel program for course                                           !
+! 530007 SE+UE Introduction to High Level                              !
+! Programming for Chemistry Students (2021S)                           !
+! (c) Markus Oppel/University of Vienna 2021                           !
+!                                                                      !
+! This file is part of Hueckel.                                        !
+!                                                                      !
+! Hueckel is free software; you can redistribute it and/or modify      !
+! it under the terms of the GNU Lesser General Public License, v. 2.1. !
+! Hueckel is distributed in the hope that it will be useful, but it    !
+! is provided "as is" and without any express or implied warranties.   !
+! For more details see the full text of the license in the file        !
+! LICENSE or in <http://www.gnu.org/licenses/>.                        !
+!                                                                      !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 program hueckel
 use global
 implicit none

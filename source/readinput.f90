95d754918ca649a4a10e35edfc8324dd10091f13
Markus Oppel <markus.oppel@univie.ac.at>
2021-05-19 11:42:16 +0200

    Update readinput.f90

--- source/readinput.f90
+++ source/readinput.f90
@@ -1,3 +1,15 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!                                                                      !
+! This file is part of Hueckel.                                        !
+!                                                                      !
+! Hueckel is free software; you can redistribute it and/or modify      !
+! it under the terms of the GNU Lesser General Public License, v. 2.1. !
+! Hueckel is distributed in the hope that it will be useful, but it    !
+! is provided "as is" and without any express or implied warranties.   !
+! For more details see the full text of the license in the file        !
+! LICENSE or in <http://www.gnu.org/licenses/>.                        !
+!                                                                      !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 subroutine readinput(natoms,title)
 use global
 implicit none
